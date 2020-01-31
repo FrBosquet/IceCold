632a4f41d00eff9ea74fda8ea69d03ef19b959f9
Fran Bosquet <frbosquet@gmail.com>
2020-01-31 18:58:25 +0100

    Add destructable rocks

---
+++ Assets/Scripts/DestructableOnImpact.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructableOnImpact : MonoBehaviour
+{
+  public float requiredEnergy;
+
+  private new BoxCollider2D collider;
+  private ParticleSystem particles;
+  private SpriteRenderer sprite;
+
+  private void Awake()
+  {
+    collider = gameObject.GetComponent<BoxCollider2D>();
+    particles = GetComponentInChildren<ParticleSystem>();
+    sprite = gameObject.GetComponent<SpriteRenderer>();
+  }
+
+  private void OnCollisionEnter2D(Collision2D other)
+  {
+    Rigidbody2D rigidbody = other.gameObject.GetComponent<Rigidbody2D>();
+    Vector2 velocity = other.relativeVelocity;
+
+    Vector2 axis = other.gameObject.transform.position - transform.position;
+
+    if (axis.x > axis.y)
+    {
+      axis = Vector2.right;
+    }
+    else
+    {
+      axis = Vector2.up;
+    }
+
+    float energy = Mathf.Abs(Vector2.Dot(axis, velocity) / axis.magnitude);
+
+    if (energy > requiredEnergy)
+    {
+      collider.enabled = false;
+      sprite.enabled = false;
+      particles.Play();
+      rigidbody.velocity = velocity.normalized * (energy - requiredEnergy);
+      StartCoroutine(WaitAndDestroy());
+    }
+
+  }
+
+  IEnumerator WaitAndDestroy()
+  {
+    yield return new WaitForSeconds(1);
+    Destroy(gameObject);
+  }
+}
