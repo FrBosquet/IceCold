7a737c42f5b3500c4a9ad8ece4c8132eca5aff2a
Fran Bosquet <frbosquet@gmail.com>
2020-02-02 12:20:29 +0100

    Add trapdoors

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+  private bool open = false;
+  private Animator animator;
+  private new BoxCollider2D collider;
+
+  private void Start()
+  {
+    animator = gameObject.GetComponent<Animator>();
+    collider = gameObject.GetComponent<BoxCollider2D>();
+  }
+
+  public void Open()
+  {
+    open = true;
+    collider.enabled = false;
+    animator.SetBool("open", open);
+  }
+
+  public void Close()
+  {
+    open = false;
+    collider.enabled = true;
+    animator.SetBool("open", open);
+  }
+}
