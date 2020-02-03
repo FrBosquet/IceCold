0d95e9c76f9074854a6dbf2559ef2f3b189fb061
Fran Bosquet <frbosquet@gmail.com>
2020-02-03 11:08:56 +0100

    Add jumping plates

---
+++ Assets/Scripts/JumpingPlate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpingPlate : MonoBehaviour
+{
+  public float force;
+
+  private void OnTriggerEnter2D(Collider2D other)
+  {
+    if (other.gameObject.CompareTag("Ball"))
+    {
+
+      Rigidbody2D ball = other.gameObject.GetComponent<Rigidbody2D>();
+
+      ball.AddForce(transform.up * force, ForceMode2D.Impulse);
+    }
+  }
+}
