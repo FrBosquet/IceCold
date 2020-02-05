e8b3da29442a547cdc2cc65cfcc9a5c6fb63d59b
Fran Bosquet <frbosquet@gmail.com>
2020-02-05 09:56:48 +0100

    Add build spots to rock ramps

---
+++ Assets/Scripts/Objects/RockRamp.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockRamp : BaseScript
+{
+  public Vector2 direction;
+  public bool hasBuildSpot;
+
+  private void Start()
+  {
+    if (hasBuildSpot) gameManager.AddBuildSpot((Vector2)transform.position + direction.normalized / 2, direction);
+  }
+}
